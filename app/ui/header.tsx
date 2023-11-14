e8926609654ab891d09c4c18713696d02298ada7
Rishabh Pawar <rishabhpawar1409@gmail.com>
2023-11-14 15:56:04 +0530

    create header,footer

--- app/ui/header.tsx
+++ app/ui/header.tsx
@@ -1,9 +1,13 @@
-
+import styles from "./header.module.css";
 export default function Header() {
-    return (
-      <div>
-          Header
+  return (
+    <div className={styles.headerContainer}>
+      <div className={styles.logoContainer}>
+        <p className={styles.logo}>Multibagg</p>
       </div>
-    )
-  }
-  
+      <div className={styles.loginContainer}>
+        <p className={styles.loginText}>login</p>
+      </div>
+    </div>
+  );
+}
