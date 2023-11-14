93c943a1e27fe5f1de0b640c8a2c483c57ab1039
Aman Dubey <107190448+Aman-Dubey-01@users.noreply.github.com>
2023-11-14 17:06:48 +0530

    Revert "Update dashboard layout, profile,users section"

--- app/dashboard/profile/page.tsx
+++ app/dashboard/profile/page.tsx
@@ -1,46 +1,9 @@
-// pages/profile.js
-import { useEffect, useState } from "react";
-import classes from "@/app/dashboard/profile/profile.module.css";
 
-
-
-const Profile = () => {
-  // const [userData, setUserData] = useState(null);
-
-  const userData = {
-    name: "John Doe",
-    image: "https://d2cbg94ubxgsnp.cloudfront.net/Pictures/2000xAny/9/9/2/512992_shutterstock_715962319converted_749269.png",
-    bio: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. In omnis quidem voluptates officia. Porro ratione soluta dolore inventore corrupti laborum iste quaerat possimus, vero magni sapiente ut unde asperiores? Aspernatur."
-  };
-
-  // useEffect(() => {
-  //   // Fetch user data from the API route
-  //   const fetchUserData = async () => {
-  //     try {
-  //       const response = await fetch("/api/profile");
-  //       const data = await response.json();
-  //       setUserData(data);
-  //     } catch (error) {
-  //       console.error("Error fetching user data:", error);
-  //     }
-  //   };
-
-  //   fetchUserData();
-  // }, []);
-
-  return (
-    <div className={classes.profile}>
-      {userData ? (
-        <div>
-          <img src={userData.image} alt="Profile" />
-          <h1>{userData.name}</h1>
-          <p>{userData.bio}</p>
-        </div>
-      ) : (
-        <p>Loading...</p>
-      )}
-    </div>
-  );
-};
-
-export default Profile;
+export default function Profile() {
+    return (
+      <div>
+          User profile
+      </div>
+    )
+  }
+  
--- app/dashboard/users/page.tsx
+++ app/dashboard/users/page.tsx
@@ -1,47 +1,8 @@
-// pages/profile.js
-// import { useEffect, useState } from "react";
 
-const Users = () => {
-  // const [users, setUsers] = useState([]);
-
-  const users = [
-    { id: 1, name: "John Doe", image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS39PZmMFkK6rrYtI-9lvgfcggaiCPTAnORjA&usqp=CAU", bio: "Web Developer" },
-    { id: 2, name: "Jane Smith", image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSKgGvJvUx7sg5j6IWlLHQ2AmCC3Ct3smEuwg&usqp=CAU", bio: "Designer" },
-    
-  ];
-
-  // useEffect(() => {
-  //   const fetchUsers = async () => {
-  //     try {
-  //       const response = await fetch("/api/profile");
-  //       const data = await response.json();
-  //       setUsers(data);
-  //     } catch (error) {
-  //       console.error("Error fetching user data:", error);
-  //     }
-  //   };
-
-  //   fetchUsers();
-  // }, []);
-
+export default function Users() {
   return (
     <div>
-      <h1>Users</h1>
-      <div className="user-list">
-        {users.map((user) => (
-          <div key={user.id} className="user-card">
-            <img src={user.image} alt={user.name} />
-            <div>
-              <h2>{user.name}</h2>
-              <p>{user.bio}</p>
-            </div>
-          </div>
-        ))}
-      </div>
-
-  
+        Users
     </div>
-  );
-};
-
-export default Users;
+  )
+}
